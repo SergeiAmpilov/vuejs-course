1c12c94d79354d50cff91054ac53c57943d4c655
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-13 23:08:43 +0300

    lesson 3: more directives

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const App = {
   data: () => ({
     title: 'I am Grut',
-    myHtml: '<h1>Vue 3 App</h1>'
+    myHtml: '<h1>Vue 3 App</h1>',
+    person: {
+      firstName: 'Sergei',
+      lastName: 'Ampilov',
+      age: 38
+    },
+    items: [1, 2, 3, 4, 5, 6],
   }),
+  methods: {
+    // hanfff
+  }
 };
 
 
