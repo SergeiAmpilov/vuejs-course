79be3830fbafc0d06c5f36d0ef7c845cfde83ccd
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-19 22:01:40 +0300

    create virtual dom

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
+const h = Vue.h;
 const App = {
-  template: `
-    <div class="card center">This content from JS template</div>
-    
-  `,
+  data() {
+    return {
+      title: 'Hello world title',
+    };
+  },
+  methods: {
+    changeTitle() {
+      console.log(this);
+      this.title += 'h';
+    }
+  },
+  render() {
+    return h(
+      'div',
+      {
+        className: 'card center'
+      }, [
+        h('h1', {}, this.title),
+        h('button', {
+          className: 'btn',
+          onClick: this.changeTitle
+        }, 'Change title')
+
+      ]
+    );
+  }
+  // template: `
+  //   <div class="card center">
+  //     <h1>{{ title }}</h1>
+  //     <p>This content from JS template</p>
+  //     <button class='btn' @click="title += 'h'">Change title</button>
+  //   </div>
+  // `,
 };
 
 const vApp = Vue.createApp(App);
